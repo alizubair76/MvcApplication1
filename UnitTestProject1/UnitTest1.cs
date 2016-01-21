eff97a11feaf573588ff353f99b89703db42fea3
Zubair Ali <alizubair76@gmail.com>
2016-01-21 09:29:14 +0530

    Push #13

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace UnitTestProject1
         [Test]
         public void TestMethod1()
         {
-            ; string abc = "" + DataAccess.DataAccess.ExecuteScalar("Select Top 1 EmployeeID From Employee");
+            zdsidsvsdovisstring abc = "" + DataAccess.DataAccess.ExecuteScalar("Select Top 1 EmployeeID From Employee");
 
             var xyz = ActMgr.GetEmployee();
 
